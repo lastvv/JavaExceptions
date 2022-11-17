de08f6f77e0baee42bae4e590a4b0bada6382c91
lastvv <103041546+lastvv@users.noreply.github.com>
2022-11-17 15:09:49 +0300

    01

---
+++ 01.java
@@ -0,0 +1,33 @@
+//Реализовал 3 метода, чтобы в каждом из них получить разные исключения
+
+package 01;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String[] sArray = {"first str", null, "third str"};
+        int[] array = {1,2,3,4,5,6,7,8,9,10,11,12};
+        division(10, 0);
+        printValueByIndex(array,12);
+        printStringArray(sArray);
+
+    }
+    // деление на ноль
+    public static void division(double firstNum, double secondNum){
+        if(secondNum ==  0) throw new ArithmeticException("На ноль делить нельзя, вообще то!!!");
+        double result = firstNum/secondNum;
+        System.out.printf("Результат деления %f на %f равен %f.\n", firstNum, secondNum, result);
+    }
+    // за пределами дозволенного
+    public static void printValueByIndex(int[] array, int index){
+        if(index > array.length-1) throw new ArrayIndexOutOfBoundsException("Элемента под таким индексом не существует");
+        else System.out.printf("Элемент с индексом %d равен %d.\n",index, array[index]);
+    }
+
+    // Исключение нулевого указателя
+    public static void printStringArray(String[] sArray){
+        for (String item: sArray){
+            if(item == null) throw new NullPointerException("увы, но элемент отсутствует!!!");
+            System.out.println(item);
+        }
+    }
+}
